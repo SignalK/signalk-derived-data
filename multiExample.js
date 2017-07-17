78525c542429cbca98610f7b7a96e15ddfa96b05
Scott Bender <scott@scottbender.net>
2017-07-16 23:34:21 -0400

    remove: debug statements

--- multiExample.js
+++ multiExample.js
@@ -7,14 +7,12 @@ module.exports = function(app, plugin) {
   if ( _.get(app.signalk.self, "electrical.batteries") )
   {
     batteries = _.keys(app.signalk.self.electrical.batteries)
-    debug("batteries: " + JSON.stringify(batteries))
-    debug("instances: " + _.keys(app.signalk.self.electrical.batteries))
     
     return batteries.map(instance => {
       return {
         optionKey: 'batterPower' + instance,
         title: "Battery " + instance + " Power ",
-        derivedFrom: function(){ return [ "electrical.batteries." + instance + ".voltage"]}, // "electrical.batteries." + instance + ".current" ] },
+        derivedFrom: function() { return [ "electrical.batteries." + instance + ".voltage", "electrical.batteries." + instance + ".current" ] },
         calculator: function(v, a) {
           return [{ path: "electrical.batteries." + instance + ".power", value: v*10 }]
         }
