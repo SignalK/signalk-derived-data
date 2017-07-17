5bc1e0a6dbab27937bf94ce0df5ac4a68f35718f
joabakk <github@heiamoss.com>
2017-07-17 21:25:48 +0200

    Add vmg_wind, correct typo in vmg

---
+++ calcs/vmg_wind.js
@@ -0,0 +1,21 @@
+
+module.exports = function(app) {
+  return {
+    group: 'vmg',
+    optionKey: 'vmg_Wind',
+    title: "Velocity Made Good to wind (based on wind.directionTrue and speedOverGround)",
+    derivedFrom: [ "environment.wind.directionTrue",
+                   "navigation.speedOverGround" ],
+    calculator: function (trueWindAngle, speedOverGround)
+    {
+      var vmg_wind = Math.cos(trueWindAngle) * speedOverGround
+      if (vmg_wind < 0){
+        return [{ path: "performance.velocityMadeGood", value: vmg_wind},
+                {path: "performance.gybeAngleVelocityMadeGood",  value: vmg_wind}]
+      } else {
+        return [{ path: "performance.velocityMadeGood", value: vmg_wind},
+                {path: "performance.beatAngleVelocityMadeGood", value: vmg_wind}]
+      }
+    }
+  };
+}
