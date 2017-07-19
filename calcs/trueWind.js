97015dd7d6937a03248bc33c96fbc88d7a2f30ea
Scott Bender <scott@scottbender.net>
2017-07-19 08:04:52 -0400

    fix: TWA TWD fixes

--- calcs/trueWind.js
+++ calcs/trueWind.js
@@ -4,7 +4,7 @@ module.exports = function(app) {
   return {
     group: 'wind',
     optionKey: 'trueWind',
-    title: "True Wind Angle and Speed (based on speed through water, AWA and AWS)",
+    title: "True Wind Angle, Direction and Speed (based on speed through water, AWA and AWS)",
     derivedFrom: [ "navigation.headingTrue", "navigation.speedThroughWater", "environment.wind.speedApparent", "environment.wind.angleApparent" ],
     calculator: function(headTrue, speed, aws, awa) {
       var apparentX = Math.cos(awa) * aws;
@@ -12,11 +12,13 @@ module.exports = function(app) {
       var angle = Math.atan2(apparentY, -speed + apparentX);
       var speed = Math.sqrt(Math.pow(apparentY, 2) + Math.pow(-speed + apparentX, 2));
 
-      if ( angle > Math.PI/2 ) {
-        angle = angle - Math.PI
-      }
-      
       var dir = headTrue + angle
+
+      if ( dir > Math.PI*2 ) {
+        dir = dir - Math.PI*2;
+      } else if ( dir < 0 ) {
+        dir = dir + Math.PI*2;
+      }
       
       return [{ path: "environment.wind.directionTrue", value: dir},
               { path: "environment.wind.angleTrueWater", value: angle},
