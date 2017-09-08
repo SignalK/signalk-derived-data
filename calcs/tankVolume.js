4275b66a5bb39fece7758d7b4f2286ae07ac760c
joabakk <github@heiamoss.com>
2017-09-08 17:37:52 +0200

    Add calibration to schema

--- calcs/tankVolume.js
+++ calcs/tankVolume.js
@@ -1,54 +1,52 @@
 var spline = require("cubic-spline")
 const _ = require("lodash")
-const debug = require('debug')('signalk-derived-data')
+const util = require('util') //dev
+const debug = require('debug')('derived-tank')
 
+
+
+
+//tankInstances = ["tanks.fuel.*", "tanks.fuel.1", "tanks.water.0" ]
 module.exports = function(app, plugin) {
-  var tankInstances
+  return {
+    group: 'tanks',
+    optionKey: 'tankVolume',
+    title: ".currentVolume (based on currentLevel (requires calibration points (>2 for parallell sides, >3 for straight wedge and >4 for more complex shapes))",
+    derivedFrom: function(){ return [  plugin.properties.tank_instance + ".currentLevel" ] },
+    properties: {
+      tank_instance: {
+        type: "string",
+        title: "Tank Instance",
+        default: "tanks.fuel.0",
+      },
 
-  if ( _.get(app.signalk.self, "tanks") )
-  {
-    tankInstances = _.keys(app.signalk.self['tanks'])
-
-    return tankInstances.map(instance => {
-      return {
-        group: 'tanks',
-        optionKey: 'tankVolume' + instance,
-        title: "tanks." + instance + ".currentVolume (based on currentLevel (requires calibration points (>2 for parallell sides, >3 for straight wedge and >4 for more complex shapes))",
-        derivedFrom: function() { return [ "tanks." + instance + ".currentLevel"] },
-        properties:{
-          tank_instance: {
-            type: "string",
-            title: "Propulsion Instance (one at a time currently)",
-            "enum": [0,1,2]//enumm
-          }, 
-          calibration:{
-            type: "array",
-            title: "Calibration points",
-            items:{
-              title: " ",
-              type: "object",
-              properties: {
-                "level": {
-                  title: "level reading",
-                  type: "number"
-                  },
-                "volume": {
-                  title: "volume reading (litres)",
-                  type: "number"
-                }
-              }
-            }    
+      calibrations: {
+        "type": "array",
+        "title": "Calibration entries (pairs of level => volume)",
+        "items": {
+          "type": "object",
+          "required": [
+            "level",
+            "volume"
+          ],
+          "properties": {
+            "level": {
+              "type": "number",
+              "title": "level (ratio max 1)",
+              "description": " "
+            },
+            "volume": {
+              "type": "number",
+              "title": "corresponding volume (m^3)",
+              "description": " "
+            }
           }
         }
-        calculator: function(level) { // and arrays of volumes and levels
-          return [{ path: 'tanks.' + instance + '.currentVolume', value: spline(level, levelArray, volumeArray)}]
-        }
       }
-    });
-  } else {
-    return {
-      optionKey: 'tankVolume',
-      title: "tanks.*.*.currentVolume (based on tanks.*.*.currentLevel)"
+    },
+    calculator: function(level) { // and arrays of volumes and levels
+      var inst = plugin.properties.tank_instance
+      return [{ path: inst + '.currentVolume', value: spline(level, [0,2,4], [0,1,2])}]//..calibrations.level, ..calibrations.volume)}]
     }
   }
 }
