4bd62ea94154e41989eb6b74c0b1b3e2e733bc2e
Scott Bender <scott@scottbender.net>
2017-09-08 14:04:00 -0400

    add: calculate battery power

---
+++ calcs/batteryPower.js
@@ -0,0 +1,27 @@
+const _ = require('lodash')
+const debug = require('debug')('signalk-derived-data')
+
+module.exports = function(app, plugin) {
+  var batteries
+
+  if ( _.get(app.signalk.self, "electrical.batteries") )
+  {
+    batteries = _.keys(app.signalk.self.electrical.batteries)
+    debug("batteries: " + JSON.stringify(batteries))
+    debug("instances: " + _.keys(app.signalk.self.electrical.batteries))
+    
+    return batteries.map(instance => {
+      return {
+        group: 'electrical',
+        optionKey: 'batterPower' + instance,
+        title: "Battery " + instance + " Power ",
+        derivedFrom: function(){ return [ "electrical.batteries." + instance + ".voltage",  "electrical.batteries." + instance + ".current" ] },
+        calculator: function(v, a) {
+          return [{ path: "electrical.batteries." + instance + ".power", value: v*a }]
+        }
+      }
+    });
+  } else {
+    return undefined
+  }
+}
