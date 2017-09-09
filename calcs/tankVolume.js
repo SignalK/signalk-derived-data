c780c37798ef684e57e24b86c440cdf6b636e38b
Joachim Bakke <github@heiamoss.com>
2017-09-10 01:55:41 +0200

    Fix Tankcalibration (#10)
    * WIP multiple instances
    
    * WIP not calculating, multiple instances, but
    
    only when in defaults.json, I believe
    Might be due to simulator starting late as well
    
    * WIP add calibration per instance
    
    Still not calculating…
    
    * Working tankVolume

--- calcs/tankVolume.js
+++ calcs/tankVolume.js
@@ -3,72 +3,87 @@ const _ = require("lodash")
 const util = require('util') //dev
 const debug = require('debug')('derived-tank')
 
-
-
+var instance
 
 //tankInstances = ["tanks.fuel.*", "tanks.fuel.1", "tanks.water.0" ]
 module.exports = function(app, plugin) {
-  return {
-    group: 'tanks',
-    optionKey: 'tankVolume',
-    title: "Tank Volume (based on currentLevel (requires calibration pairs (>2 for parallell sides, >3 for straight wedge and >4 for more complex shapes))",
-    derivedFrom: function(){
-      return [  plugin.properties.tanks.tank_instance + ".currentLevel" ] },
-    properties: function() {
-      var tank_types = _.keys(app.signalk.self.tanks)
-      var instances = []
-      tank_types.forEach(type => {
-        _.keys(app.signalk.self.tanks[type]).forEach(i => {
-          instances.push("tanks." + type + "." + i)
-        })
+  var instances = []
+  debug(_.keys(app.signalk.self.tanks))
+  if ( _.get(app.signalk.self, "tanks") ){
+    debug("tanks found")
+    var tank_types = _.keys(app.signalk.self.tanks)
+    tank_types.forEach(type => {
+      debug(type)
+      _.keys(app.signalk.self.tanks[type]).forEach(i => {
+        debug(i)
+        instances.push("tanks." + type + "." + i)
       })
-
-      instance_prop = {
-        type: "string",
-        title: "Tank Instance",
-      }
-
-      if ( instances.length > 0 )
-        instance_prop.enum = instances
-      
+    })
+    return instances.map(instance => {
       return {
-        tank_instance: instance_prop,
-        calibrations: {
-          "type": "array",
-          "title": "Calibration entries (pairs of level => volume)",
-          "items": {
-            "type": "object",
-            "required": [
-              "level",
-              "volume"
-            ],
-            "properties": {
-              "level": {
-                "type": "number",
-                "title": "level (ratio max 1)",
-                "description": " "
-              },
-              "volume": {
-                "type": "number",
-                "title": "corresponding volume (m^3)",
-                "description": " "
+        group: 'tanks',
+        optionKey: 'tankVolume_' + instance,
+        title: "'" + instance + "' Tank Volume (based on currentLevel (requires calibration pairs (>2 for parallell sides, >3 for straight wedge and >4 for more complex shapes))",
+        derivedFrom: function(){
+          return [  instance + ".currentLevel" ]
+        },
+        properties: {
+          volume_unit: {
+            type: "string",
+            title: "Input unit",
+            enum: ["litres", "gal", "m3"],
+            default: "litres"
+          },
+          ["calibrations."+instance]: {
+            "type": "array",
+            "title": "Calibration entries (pairs of level => volume)",
+            "items": {
+              "type": "object",
+              "required": [
+                "level",
+                "volume"
+              ],
+              "properties": {
+                "level": {
+                  "type": "number",
+                  "title": "level (ratio max 1)",
+                  "description": " "
+                },
+                "volume": {
+                  "type": "number",
+                  "title": "corresponding volume (selected unit)",
+                  "description": " "
+                }
               }
             }
           }
+        },
+        calculator: function(level) {
+
+          var calLevels = []
+          var calVolumes = []
+          debug(plugin.properties.tanks.volume_unit)
+
+          plugin.properties.tanks["calibrations."+instance].forEach(function(i) {
+            calLevels.push(i.level)
+            if (plugin.properties.tanks.volume_unit === "litres"){
+              calVolumes.push(i.volume*0.001)
+            } else if (plugin.properties.tanks.volume_unit === "gal") {
+              calVolumes.push(i.volume*0.00378541)
+            } else {
+              calVolumes.push(i.volume)
+            }
+          })
+
+          return [{ path: instance + '.currentVolume', value: spline(level, calLevels, calVolumes)}]
         }
       }
-    },
-    calculator: function(level) {
-      var inst = plugin.properties.tanks.tank_instance
-      var calLevels = []
-      calVolumes = []
-
-      plugin.properties.tanks.calibrations.forEach(function(i) {
-        calLevels.push(i.level)
-        calVolumes.push(i.volume)
-      });
-
-      return [{ path: inst + '.currentVolume', value: spline(level, calLevels, calVolumes)}]
+    })
+  } else {
+    return {
+      group: 'tanks',
+      optionKey: 'tankVolume_',
+      title: "Tank Volume (based on currentLevel (no tank levels found))",
     }
   }
 }
