e256c8221826601c5248adbd9b67333d527e6128
joabakk <github@heiamoss.com>
2017-10-16 01:18:46 +0200

    Fix to windchill calc

--- calcs/windChill.js
+++ calcs/windChill.js
@@ -8,14 +8,17 @@ module.exports = function(app, plugin) {
     derivedFrom: [ "environment.outside.temperature", "environment.wind.speedApparent" ],
     calculator: function(temp, windSpeed) {
       //standard Wind Chill formula for Environment Canada
-      const tempC = temp + 273.16
-      const windSpeedKmh = windSpeed * 3600 / 1000
+      const tempC = temp - 273.16
+      const windSpeedKt = windSpeed * 1.852
+      var windChill=13.12 + 0.6215*tempC - 11.37*Math.pow(windSpeedKt,0.16) + 0.3965*tempC*Math.pow(windSpeedKt,0.16)+273.16
 
-      var windChill = (13.12 + (0.6215 * tempC) - (11.37 * windSpeedKmh^0.16) + (0.3965 * tempC * windSpeedKmh^0.16)) - 273.16
       //Please Note: The calculator should not be used for outside air temperatures greater that 10 °C (50 °F ) and wind speeds less than 4.8 Km/hr
-      if(tempC < 10 && windSpeedKmh > 4.8){
-        return [{ path: "environment.outside.apparentWindChillTemperature", value: windChill}]
-      } else return
+      windChill = (windSpeedKt <= 4.8) ? tempC+273.16 : windChill;
+      windChill = (tempC > 10) ? tempC+273.16 : windChill;
+
+
+      return [{ path: "environment.outside.apparentWindChillTemperature", value: windChill}]
+
     }
   };
 }
