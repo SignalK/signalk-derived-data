2f3510a9127fa4213095a6ceae7fad1debc649b9
Scott Bender <scott@scottbender.net>
2017-12-24 20:06:22 -0500

    feature: use system time if navigation.datetime is not available

--- calcs/suncalc.js
+++ calcs/suncalc.js
@@ -1,4 +1,4 @@
-var debug = require('debug')('signalk-derived-data')
+var debug = require('debug')('signalk-derived-data:sun')
 const suncalc = require('suncalc');
 const _ = require('lodash')
 
@@ -7,12 +7,22 @@ module.exports = function(app, plugin) {
   return {
     group: 'sun',
     optionKey: 'Sun',
-    title: "Sets environment.sun to dawn, sunrise, day, sunset, dusk or night",
+    title: "Sets environment.sun to dawn, sunrise, day, sunset, dusk or night (based on navigation.datetime or system time and navigation.position)",
     derivedFrom: [ "navigation.datetime", "navigation.position" ],
+    defaults: [ '', undefined ],
+    debounceDelay: 60*1000,
     calculator: function(datetime, position) {
       var value
+      var date
 
-      var times = suncalc.getTimes(new Date(datetime), position.latitude, position.longitude)
+      if ( datetime && datetime.length > 0 ) {
+        date = new Date(datetime)
+      } else {
+        date = new Date()
+      }
+        
+
+      var times = suncalc.getTimes(date, position.latitude, position.longitude)
       var now = new Date().getTime()
 
       _.keys(times).forEach(key => {
@@ -38,6 +48,8 @@ module.exports = function(app, plugin) {
           value = 'night'
         }
       }
+
+      debug(`Setting sun to ${value}`)
       
       return [{ path: "environment.sun", value: value}]
     }
