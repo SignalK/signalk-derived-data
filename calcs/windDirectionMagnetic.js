c1b32545753756b9bb4db57a76e7ddcb4fb6ae59
Scott Bender <scott@scottbender.net>
2018-03-23 22:49:12 -0400

    fix: Magnetic Wind Direction calculation to wrap correctly

--- calcs/windDirectionMagnetic.js
+++ calcs/windDirectionMagnetic.js
@@ -7,7 +7,13 @@ module.exports = function(app, plugin) {
     title: "Magnetic Wind Direction (based on wind.directionTrue and magenticVarition)",
     derivedFrom: [ "environment.wind.directionTrue", "navigation.magneticVariation" ],
     calculator: function(directionTrue, magneticVariation) {
-      return [{ path: "environment.wind.directionMagnetic", value: directionTrue + magneticVariation}]
+      var directionMagnetic = directionTrue + magneticVariation
+      if ( directionMagnetic < 0 ) {
+        directionMagnetic = (Math.PI*2) + directionMagnetic
+      } else if ( directionMagnetic > (Math.PI*2) ) {
+        directionMagnetic = directionMagnetic - (Math.PI*2)
+      }
+      return [{ path: "environment.wind.directionMagnetic", value: directionMagnetic}]
     }
   };
 }
