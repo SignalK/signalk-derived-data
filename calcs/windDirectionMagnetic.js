4b81454e1c7a464cd90dd38994d51a935b8fab37
Scott Bender <scott@scottbender.net>
2018-03-23 20:31:10 -0400

    fix: bad math for Magnetic Wind Direction

--- calcs/windDirectionMagnetic.js
+++ calcs/windDirectionMagnetic.js
@@ -7,7 +7,7 @@ module.exports = function(app, plugin) {
     title: "Magnetic Wind Direction (based on wind.directionTrue and magenticVarition)",
     derivedFrom: [ "environment.wind.directionTrue", "navigation.magneticVariation" ],
     calculator: function(directionTrue, magneticVariation) {
-      return [{ path: "environment.wind.directionMagnetic", value: directionTrue - magneticVariation}]
+      return [{ path: "environment.wind.directionMagnetic", value: directionTrue + magneticVariation}]
     }
   };
 }
