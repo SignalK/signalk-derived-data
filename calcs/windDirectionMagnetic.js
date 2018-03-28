bb102badd1336e4337d18f7aff4056b9bb5ab1b9
Scott Bender <scott@scottbender.net>
2018-03-28 17:05:10 -0400

    fix: subtract magneticVariation to from directionTrue to get directionMagnetic

--- calcs/windDirectionMagnetic.js
+++ calcs/windDirectionMagnetic.js
@@ -4,10 +4,10 @@ module.exports = function(app, plugin) {
   return {
     group: 'wind',
     optionKey: 'directionMagnetic',
-    title: "Magnetic Wind Direction (based on wind.directionTrue and magenticVarition)",
+    title: "Magnetic Wind Direction (based on wind.directionTrue and magneticVarition)",
     derivedFrom: [ "environment.wind.directionTrue", "navigation.magneticVariation" ],
     calculator: function(directionTrue, magneticVariation) {
-      var directionMagnetic = directionTrue + magneticVariation
+      var directionMagnetic = directionTrue - magneticVariation
       if ( directionMagnetic < 0 ) {
         directionMagnetic = (Math.PI*2) + directionMagnetic
       } else if ( directionMagnetic > (Math.PI*2) ) {
