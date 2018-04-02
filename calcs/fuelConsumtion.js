11fec072a8cef8f69c85cbce4be0cd62757f2dff
Scott Bender <scott@scottbender.net>
2018-04-02 19:31:43 -0400

    feature: add fuel econmony

---
+++ calcs/fuelConsumtion.js
@@ -0,0 +1,20 @@
+const _ = require('lodash')
+
+module.exports = function(app, plugin) {
+  var engines = plugin.engines
+
+  app.debug('engines: %j', engines)
+
+  return engines.map(instance => {
+    return {
+      group: 'propulsion',
+      optionKey: 'economy' + instance,
+      title: `${instance} fuel economy (based on speed over ground, fuel rate)`,
+      derivedFrom: function(){ return [ "propulsion." + instance + ".fuel.rate",  "navigation.speedOverGround" ] },
+      calculator: function(rate, speed) {
+        return [{ path: "propulsion." + instance + ".fuel.economy",
+                  value: speed/rate }]
+      }
+    }
+  });
+}
