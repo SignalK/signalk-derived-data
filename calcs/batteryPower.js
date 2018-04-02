63353ceaae7c9a44ba61a58aacd3175ae0a69f1e
Scott Bender <scott@scottbender.net>
2018-04-02 19:31:02 -0400

    fix: add confirable battery, engine and tank instances since we don't know them at startup

--- calcs/batteryPower.js
+++ calcs/batteryPower.js
@@ -1,26 +1,15 @@
 const _ = require('lodash')
 
 module.exports = function(app, plugin) {
-  var batteries
-
-  if ( app.getSelfPath("electrical.batteries") )
-  {
-    batteries = _.keys(app.signalk.self.electrical.batteries)
-    app.debug("batteries: " + JSON.stringify(batteries))
-    app.debug("instances: " + _.keys(app.signalk.self.electrical.batteries))
-    
-    return batteries.map(instance => {
-      return {
-        group: 'electrical',
-        optionKey: 'batterPower' + instance,
-        title: "Battery " + instance + " Power ",
-        derivedFrom: function(){ return [ "electrical.batteries." + instance + ".voltage",  "electrical.batteries." + instance + ".current" ] },
-        calculator: function(v, a) {
-          return [{ path: "electrical.batteries." + instance + ".power", value: v*a }]
-        }
+  return plugin.batteries.map(instance => {
+    return {
+      group: 'electrical',
+      optionKey: 'batterPower' + instance,
+      title: "Battery " + instance + " Power ",
+      derivedFrom: function(){ return [ "electrical.batteries." + instance + ".voltage",  "electrical.batteries." + instance + ".current" ] },
+      calculator: function(v, a) {
+        return [{ path: "electrical.batteries." + instance + ".power", value: v*a }]
       }
-    });
-  } else {
-    return undefined
-  }
+    }
+  });
 }
