0fa3e297fbebd77b15cc7c17e81549ef51824178
Scott Bender <scott@scottbender.net>
2018-07-09 10:12:20 -0400

    feature: added headingTrue calculation

---
+++ calcs/headingTrue.js
@@ -0,0 +1,20 @@
+
+
+
+module.exports = function(app, plugin) {
+  return {
+    group: 'heading',
+    optionKey: 'heading',
+    title: "True Heading (based on magnetic heading  and magneticVarition)",
+    derivedFrom: [ "navigation.headingMagnetic", "navigation.magneticVariation" ],
+    calculator: function(heading, magneticVariation) {
+      var headingTrue = heading + magneticVariation
+      if ( headingTrue < 0 ) {
+        headingTrue = (Math.PI*2) + headingTrue
+      } else if ( headingTrue > (Math.PI*2) ) {
+        headingTrue = headingTrue - (Math.PI*2)
+      }
+      return [{ path: "navigation.headingTrue", value: headingTrue}]
+    }
+  };
+}
