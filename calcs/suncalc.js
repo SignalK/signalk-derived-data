1971758081398c439816ced7f6ca8dd79773e904
Scott Bender <scott@scottbender.net>
2018-07-09 10:13:31 -0400

    feature: sun calculation now also sets environment.mode

--- calcs/suncalc.js
+++ calcs/suncalc.js
@@ -6,12 +6,13 @@ module.exports = function(app, plugin) {
   return {
     group: 'sun',
     optionKey: 'Sun',
-    title: "Sets environment.sun to dawn, sunrise, day, sunset, dusk or night (based on navigation.datetime or system time and navigation.position)",
+    title: "Sets environment.sun to dawn, sunrise, day, sunset, dusk or night. Sets environment.mode to day or night. (based on navigation.datetime or system time and navigation.position)",
     derivedFrom: [ "navigation.datetime", "navigation.position" ],
     defaults: [ '', undefined ],
     debounceDelay: 60*1000,
     calculator: function(datetime, position) {
       var value
+      var mode
       var date
 
       if ( datetime && datetime.length > 0 ) {
@@ -20,6 +21,7 @@ module.exports = function(app, plugin) {
         date = new Date()
       }
         
+      app.debug(`Using datetime: ${date} position: ${JSON.stringify(position)}`)
 
       var times = suncalc.getTimes(date, position.latitude, position.longitude)
       var now = new Date().getTime()
@@ -31,16 +33,22 @@ module.exports = function(app, plugin) {
       if ( now >= times.sunrise ) {
         if ( now < times.sunriseEnd ) {
           value = 'sunrise'
+          mode = 'day'
         } else if ( now <= times.sunsetStart ) {
           value = 'day'
+          mode = 'day'
         } else if ( now >= times.sunsetStart && now < times.dusk ) {
           value = 'sunset'
+          mode = 'night'
         } else if ( now < times.night ) {
           value = 'dusk'
+          mode = 'night'
         } else {
           value = 'night'
+          mode = 'night'
         }
       } else {
+        mode = 'night'
         if ( now >= times.dawn ) {
           value = 'dawn'
         } else {
@@ -48,9 +56,10 @@ module.exports = function(app, plugin) {
         }
       }
 
-      app.debug(`Setting sun to ${value}`)
+      app.debug(`Setting sun to ${value} and mode to ${mode}`)
       
-      return [{ path: "environment.sun", value: value}]
+      return [{ path: "environment.sun", value: value},
+              {path: 'environment.mode', value: mode}]
     }
   };
 }
