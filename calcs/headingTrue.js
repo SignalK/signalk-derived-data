1fb78b8d952fabdc4708747f2edec87e9d2c1649
Scott Bender <scott@scottbender.net>
2019-06-27 09:10:04 -0400

    feature: use magneticVariation from defaults if not available

--- calcs/headingTrue.js
+++ calcs/headingTrue.js
@@ -1,10 +1,21 @@
+const _ = require('lodash')
+
 module.exports = function (app, plugin) {
   return {
     group: 'heading',
     optionKey: 'heading',
     title: 'True Heading (based on magnetic heading  and magneticVarition)',
     derivedFrom: ['navigation.headingMagnetic', 'navigation.magneticVariation'],
+    defaults: [undefined, 9999],
     calculator: function (heading, magneticVariation) {
+      if (magneticVariation === 9999) {
+        magneticVariation = app.getSelfPath(
+          'navigation.magneticVariation.value'
+        )
+        if (_.isUndefined(magneticVariation)) {
+          return
+        }
+      }
       var headingTrue = heading + magneticVariation
       if (headingTrue < 0) {
         headingTrue = Math.PI * 2 + headingTrue
