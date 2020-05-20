44600453102c8f1bffedc91987a0ce4b0d860b2b
Fabian Tollenaar <fabian@decipher.industries>
2020-05-20 16:58:12 +0200

    Use app.getSelfPath

--- calcs/depthBelowKeel2.js
+++ calcs/depthBelowKeel2.js
@@ -1,22 +1,24 @@
 const _ = require('lodash')
 
 module.exports = function (app) {
-  let depthTransducerToKeel = app.getSelfPath('environment.depth.transducerToKeel.value') || 0
+  let depthTransducerToKeel =
+    app.getSelfPath('environment.depth.transducerToKeel.value') || 0
 
   return {
     group: 'depth',
     optionKey: 'belowKeel_2',
-    title: 'Depth Below Keel (based on depth.belowTransducer and depth.transducerToKeel)',
-    derivedFrom: ['environment.depth.belowTransducer', 'environment.depth.transducerToKeel'],
-    calculator: function (depthBelowTransducer, transducerToKeel) {
-      if (typeof transducerToKeel === 'number') {
-        depthTransducerToKeel = transducerToKeel
-      } else {
-        depthTransducerToKeel = app.getSelfPath('environment.depth.transducerToKeel.value') || 0
-      }
+    title:
+      'Depth Below Keel (based on depth.belowTransducer and depth.transducerToKeel)',
+    derivedFrom: ['environment.depth.belowTransducer'],
+    calculator: function (depthBelowTransducer) {
+      depthTransducerToKeel =
+        app.getSelfPath('environment.depth.transducerToKeel.value') || 0
 
       // Need to check if number, because 0 is a valid value but also falsy
-      if (typeof depthBelowTransducer !== 'number' || typeof depthTransducerToKeel !== 'number') {
+      if (
+        typeof depthBelowTransducer !== 'number' ||
+        typeof depthTransducerToKeel !== 'number'
+      ) {
         return undefined
       }
 
@@ -26,12 +28,7 @@ module.exports = function (app) {
         return undefined
       }
 
-      return [
-        {
-          path: 'environment.depth.belowKeel',
-          value,
-        },
-      ]
-    },
+      return [{ path: 'environment.depth.belowKeel', value }]
+    }
   }
 }
