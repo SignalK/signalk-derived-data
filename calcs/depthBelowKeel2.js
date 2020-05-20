79f1e0e1bff6ae2bf9b24ec1aac205adb239b63e
Fabian Tollenaar <fabian@decipher.industries>
2020-05-20 19:12:09 +0200

    Bugfix: retrieve depth.transducerToKeel in calculation, not at start-up (#50)
    Move transducerToKeel to inside calculator, as this value may change (lifting keel) or may not be present at server start-up.

--- calcs/depthBelowKeel2.js
+++ calcs/depthBelowKeel2.js
@@ -1,28 +1,31 @@
 const _ = require('lodash')
 
 module.exports = function (app) {
-  var depthTransducerToKeel = app.getSelfPath(
-    'environment.depth.transducerToKeel.value'
-  )
-
-  var derivedFrom =
-    typeof depthTransducerToKeel === 'undefined'
-      ? []
-      : ['environment.depth.belowTransducer']
-
   return {
     group: 'depth',
     optionKey: 'belowKeel_2',
     title:
       'Depth Below Keel (based on depth.belowTransducer and depth.transducerToKeel)',
-    derivedFrom: derivedFrom,
+    derivedFrom: ['environment.depth.belowTransducer'],
     calculator: function (depthBelowTransducer) {
-      return [
-        {
-          path: 'environment.depth.belowKeel',
-          value: depthBelowTransducer + depthTransducerToKeel
-        }
-      ]
+      const depthTransducerToKeel =
+        app.getSelfPath('environment.depth.transducerToKeel.value') || 0
+
+      // Need to check if number, because 0 is a valid value but also falsy
+      if (
+        typeof depthBelowTransducer !== 'number' ||
+        typeof depthTransducerToKeel !== 'number'
+      ) {
+        return undefined
+      }
+
+      const value = depthBelowTransducer + depthTransducerToKeel
+
+      if (isNaN(value)) {
+        return undefined
+      }
+
+      return [{ path: 'environment.depth.belowKeel', value }]
     }
   }
 }
