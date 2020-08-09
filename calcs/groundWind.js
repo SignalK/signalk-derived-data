c7231c56c1d98d4e5291324c555dc215a8da0bb7
Scott Bender <scott@scottbender.net>
2020-08-09 11:36:01 -0400

    feature: add ground wind direction

--- calcs/groundWind.js
+++ calcs/groundWind.js
@@ -1,24 +1,45 @@
 module.exports = function (app, plugin) {
-  return {
-    group: 'wind',
-    optionKey: 'groundWind',
-    title: 'Ground Wind Angle and Speed (based on SOG, AWA and AWS)',
-    derivedFrom: [
-      'navigation.speedOverGround',
-      'environment.wind.speedApparent',
-      'environment.wind.angleApparent'
-    ],
-    calculator: function (sog, aws, awa) {
-      var apparentX = Math.cos(awa) * aws
-      var apparentY = Math.sin(awa) * aws
-      var angle = Math.atan2(apparentY, -sog + apparentX)
-      var speed = Math.sqrt(
-        Math.pow(apparentY, 2) + Math.pow(-sog + apparentX, 2)
-      )
-      return [
-        { path: 'environment.wind.angleTrueGround', value: angle },
-        { path: 'environment.wind.speedOverGround', value: speed }
-      ]
+  return [
+    {
+      group: 'wind',
+      optionKey: 'groundWind',
+      title: 'Ground Wind Angle and Speed (based on SOG, AWA and AWS)',
+      derivedFrom: [
+        'navigation.speedOverGround',
+        'environment.wind.speedApparent',
+        'environment.wind.angleApparent'
+      ],
+      calculator: function (sog, aws, awa) {
+        var apparentX = Math.cos(awa) * aws
+        var apparentY = Math.sin(awa) * aws
+        var angle = Math.atan2(apparentY, -sog + apparentX)
+        var speed = Math.sqrt(
+          Math.pow(apparentY, 2) + Math.pow(-sog + apparentX, 2)
+        )
+        return [
+          { path: 'environment.wind.angleTrueGround', value: angle },
+          { path: 'environment.wind.speedOverGround', value: speed }
+        ]
+      }
+    },
+    {
+      group: 'wind',
+      optionKey: 'groundWindDirection',
+      title: 'Ground Wind Direction (based on headingTrue and GWA)',
+      derivedFrom: [
+        'navigation.headingTrue',
+        'environment.wind.angleTrueGround'
+      ],
+      calculator: function (headingTrue, gwa) {
+        let windHeading = headingTrue + gwa
+
+        if (windHeading > Math.PI * 2) windHeading -= Math.PI * 2
+        else if (windHeading < 0) windHeading += Math.PI * 2
+
+        return [
+          { path: 'environment.wind.directionGround', value: windHeading }
+        ]
+      }
     }
-  }
+  ]
 }
