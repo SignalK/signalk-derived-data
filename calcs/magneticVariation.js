6e32645b677066de91b217dd6a8b9cab9f141936
Scott Bender <scott@scottbender.net>
2020-11-26 14:26:00 -0500

    tests: add testing framework

--- calcs/magneticVariation.js
+++ calcs/magneticVariation.js
@@ -12,6 +12,14 @@ module.exports = function (app, plugin) {
       let degreesVar = MagVar.get([position.latitude], [position.longitude])
       let magVar = degreesVar * Math.PI / 180
       return [{ path: 'navigation.magneticVariation', value: magVar }]
-    }
+    },
+    tests: [
+      {
+        input: [{ latitude: 39.0631232, longitude: -76.4872768 }],
+        expected: [
+          { path: 'navigation.magneticVariation', value: -0.19355701404617112 }
+        ]
+      }
+    ]
   }
 }
