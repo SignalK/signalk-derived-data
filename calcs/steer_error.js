05032f5a8ad95c8a5dde14eda274cf687e03393d
Techstyleuk <79181587+Techstyleuk@users.noreply.github.com>
2021-02-16 21:27:17 -0600

    Create steer_error.js
    this file adds a few phrases for Steer Error, this being the difference between the desired direction and the actual direction.  there are 3 different phrases:
    
    "navigation.courseGreatCircle.nextPoint.steerError" - this gives a value for the error with a positive or negative to suggest the direction
    "navigation.courseGreatCircle.nextPoint.leftSteerError" - this gives a value for the error in the Port direction
    "navigation.courseGreatCircle.nextPoint.rightSteerError" - this gives a value for the error in the Starboard direction
    the right and left steer error are for use to help with clear race displays to help a crew member get clear direction in the heat of a race

---
+++ calcs/steer_error.js
@@ -0,0 +1,45 @@
+module.exports = function (app) {
+  return {
+    group: 'course data',
+    optionKey: 'steer_error',
+    title:
+      'Estimated steer error and direction from COGS, Bearing to next waypoint (true)',
+    derivedFrom: ['navigation.courseOverGroundTrue', 'navigation.courseRhumbline.bearingToDestinationTrue'],
+    calculator: function (courseOverGroundTrue, bearingToDestinationTrue) {
+      var steererr
+      var steer
+      var leftSteer
+      var rightSteer
+
+      steererr = courseOverGroundTrue - bearingToDestinationTrue
+      if (steererr > 3.14159265359) {
+        steer = (steererr - 3.14159265359)*-1
+      } else if (steererr < -3.14159265359) {
+        steer = (steererr + 3.14159265359)*-1
+      } else {
+        steer = steererr
+      }
+
+      if (steer > 0) {
+       leftSteer = steer,
+       rightSteer = 0
+      } else {
+       leftSteer = 0,
+       rightSteer = steer * -1
+      }
+
+     app.debug(`steer: ${steer} leftSteer: ${leftSteer} rightSteer: ${rightSteer}`)
+
+      return [
+      {
+      path: 'navigation.courseGreatCircle.nextPoint.steerError', value: steer
+         },
+      {
+      path: 'navigation.courseGreatCircle.nextPoint.leftSteerError', value: leftSteer
+         },
+      {
+      path: 'navigation.courseGreatCircle.nextPoint.rightSteerError', value: rightSteer
+         }]
+
+  }}
+}
