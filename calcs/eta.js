3c82e23f08524b38c5958df97d45d795db0cbb2d
Techstyleuk <79181587+Techstyleuk@users.noreply.github.com>
2021-03-25 09:06:38 -0500

    ETA to next waypoint and steer error (#77)
    * Create eta.js
    
    I would like to add 'eta' to next waypoint to derived data.  this file creates:
    'navigation.courseGreatCircle.nextPoint.eta' and delivers a date/time of arrival at the waypoint
    
    please let me know if I need to do anything else to add it?
    
    * Create steer_error.js
    
    this file adds a few phrases for Steer Error, this being the difference between the desired direction and the actual direction.  there are 3 different phrases:
    
    "navigation.courseGreatCircle.nextPoint.steerError" - this gives a value for the error with a positive or negative to suggest the direction
    "navigation.courseGreatCircle.nextPoint.leftSteerError" - this gives a value for the error in the Port direction
    "navigation.courseGreatCircle.nextPoint.rightSteerError" - this gives a value for the error in the Starboard direction
    the right and left steer error are for use to help with clear race displays to help a crew member get clear direction in the heat of a race

---
+++ calcs/eta.js
@@ -0,0 +1,37 @@
+module.exports = function (app) {
+  return {
+    group: 'course data',
+    optionKey: 'eta_waypoint',
+    title:
+      'Estimated time of arrival at the next waypoint using distance and VMG to course',
+    derivedFrom: ['navigation.datetime', 'navigation.courseRhumbline.nextPoint.distance', 'navigation.courseRhumbline.nextPoint.velocityMadeGood'],
+    calculator: function (datetime, distance, velocityMadeGood) {
+                var date
+
+      if (datetime && datetime.length > 0) {
+        date = new Date(datetime)
+      } else {
+        date = new Date()
+      }
+
+      var datems = date.getTime()
+      var timetopoint = Math.floor((distance / (velocityMadeGood * 0.514444)) * 1000)
+
+//      app.debug(`Using datetime: ${date} ms to point : ${timetopoint} currentms: ${datems}`)
+      var etams = datems + timetopoint
+//      app.debug(`eta in ms: ${etams} ms to point : ${timetopoint} currentms: ${datems}`)
+
+      if (velocityMadeGood > 0) {
+      var etad = new Date(parseInt(etams));
+      var eta = etad.toISOString()
+      } else {
+      var  eta = "--"
+      }
+      app.debug(`what is eta: ${eta} etams: ${etams} etad: ${etad}`)
+
+      return [
+      {
+      path: 'navigation.courseGreatCircle.nextPoint.eta', value: eta
+         }]
+  }}
+}
