75247c2494d0369fbccad37f8e0febaa693977d8
Johan Wallinder <johan.wallinder@funnel.io>
2021-06-01 14:40:41 +0200

    calculate leeway angle from HDG and COG

---
+++ calcs/leewayAngle.js
@@ -0,0 +1,17 @@
+//calculation source: https://arvelgentry.jimdo.com/app/download/9157993883/Arvel+Gentry+-+Sailboat_Performance_Testing_Techniques.pdf?t=1485748085
+module.exports = function (app, plugin) {
+  return {
+    group: "heading",
+    optionKey: "leeway",
+    title: "Leeway (based on heading and COG)",
+    derivedFrom: [
+      "navigation.headingMagnetic",
+      "navigation.courseOverGroundTrue",
+    ],
+    calculator: function (hdg, cog) {
+      var leewayAngle = Math.abs(hdg - cog);
+      app.debug("leeway angle: " + leewayAngle);
+      return [{ path: "performance.leeway", value: leewayAngle }];
+    },
+  };
+};
