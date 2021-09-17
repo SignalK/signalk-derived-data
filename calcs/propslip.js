29fb7296faf785e9dd2b82a888b0e63d63da7f52
Teppo Kurki <teppo.kurki@iki.fi>
2021-09-17 21:54:33 +0300

    fix: guard against 0 revs in propslip
    Adds also minimal test for this.

--- calcs/propslip.js
+++ calcs/propslip.js
@@ -1,5 +1,24 @@
 const _ = require('lodash')
 
+const selfData = {
+  propulsion: {
+    port: {
+      transmission: {
+        gearRatio: {
+          value: 1
+        }
+      },
+      drive: {
+        propeller: {
+          pitch: {
+            value: 1
+          }
+        }
+      }
+    }
+  }
+}
+
 module.exports = function (app, plugin) {
   return plugin.engines.map(instance => {
     return {
@@ -26,13 +45,32 @@ module.exports = function (app, plugin) {
         var pitch = app.getSelfPath(
           'propulsion.' + instance + '.drive.propeller.pitch.value'
         )
-        return [
-          {
-            path: 'propulsion.' + instance + '.drive.propeller.slip',
-            value: 1 - stw * gearRatio / (revolutions * pitch)
-          }
-        ]
-      }
+        if (revolutions > 0) {
+          return [
+            {
+              path: 'propulsion.' + instance + '.drive.propeller.slip',
+              value: 1 - stw * gearRatio / (revolutions * pitch)
+            }
+          ]
+        }
+      },
+      tests: [
+        {
+          input: [0, 1],
+          selfData,
+          expected: undefined
+        },
+        {
+          input: [2, 1],
+          selfData,
+          expected: [
+            {
+              path: 'propulsion.port.drive.propeller.slip',
+              value: 0.5
+            }
+          ]
+        }
+      ]
     }
   })
 }
