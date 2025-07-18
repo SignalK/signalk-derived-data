8e9830e5e0ab36a008885ea52f68261aa88ea659
wjquigs11 <39499364+wjquigs11@users.noreply.github.com>
2025-07-18 15:28:20 -0700

    Update trueWind.js
    Fix true wind angle (TWA) when compass direction not available.

--- calcs/trueWind.js
+++ calcs/trueWind.js
@@ -1,49 +1,88 @@
 module.exports = function (app) {
-  return {
-    group: 'wind',
-    optionKey: 'trueWind',
-    title: 'True Wind Angle, Direction and Speed',
-    derivedFrom: [
-      'navigation.headingTrue',
-      'navigation.speedThroughWater',
-      'environment.wind.speedApparent',
-      'environment.wind.angleApparent'
-    ],
-    calculator: function (headTrue, speed, aws, awa) {
-      var angle
-      var speed
-      var dir
+  return [
+    {
+      group: 'wind',
+      optionKey: 'trueWind',
+      title: 'True Wind Angle, Direction and Speed',
+      derivedFrom: [
+        'navigation.headingTrue',
+        'navigation.speedThroughWater',
+        'environment.wind.speedApparent',
+        'environment.wind.angleApparent'
+      ],
+      calculator: function (headTrue, speed, aws, awa) {
+        var angle
+        var speed
+        var dir
 
-      if (headTrue == null || speed == null || aws == null || awa == null) {
-        angle = null
-        speed = null
-        dir   = null
-      } else {
-        var apparentX = Math.cos(awa) * aws
-        var apparentY = Math.sin(awa) * aws
-        angle = Math.atan2(apparentY, -speed + apparentX)
-        speed = Math.sqrt(
-          Math.pow(apparentY, 2) + Math.pow(-speed + apparentX, 2)
-        )
+        if (headTrue == null || speed == null || aws == null || awa == null) {
+          angle = null
+          speed = null
+          dir   = null
+        } else {
+          var apparentX = Math.cos(awa) * aws
+          var apparentY = Math.sin(awa) * aws
+          angle = Math.atan2(apparentY, -speed + apparentX)
+          speed = Math.sqrt(
+            Math.pow(apparentY, 2) + Math.pow(-speed + apparentX, 2)
+          )
 
-        if (aws < 1e-9) {
-          angle = awa
+          if (aws < 1e-9) {
+            angle = awa
+          }
+
+          dir = headTrue + angle
+
+          if (dir > Math.PI * 2) {
+            dir = dir - Math.PI * 2
+          } else if (dir < 0) {
+            dir = dir + Math.PI * 2
+          }
         }
 
-        dir = headTrue + angle
-
-        if (dir > Math.PI * 2) {
-          dir = dir - Math.PI * 2
-        } else if (dir < 0) {
-          dir = dir + Math.PI * 2
-        }
+        return [
+          { path: 'environment.wind.directionTrue', value: dir },
+          { path: 'environment.wind.angleTrueWater', value: angle },
+          { path: 'environment.wind.speedTrue', value: speed }
+        ]
       }
+    },
+    {
+      group: 'wind',
+      optionKey: 'trueWind',
+      title: 'True Wind Angle and Speed',
+      derivedFrom: [
+        'navigation.speedThroughWater',
+        'environment.wind.speedApparent',
+        'environment.wind.angleApparent'
+      ],
+      calculator: function (speed, aws, awa) {
+        var angle
+        var speed
+        var dir
 
-      return [
-        { path: 'environment.wind.directionTrue', value: dir },
-        { path: 'environment.wind.angleTrueWater', value: angle },
-        { path: 'environment.wind.speedTrue', value: speed }
-      ]
+        if (speed == null || aws == null || awa == null) {
+          angle = null
+          speed = null
+          dir   = null
+        } else {
+          var apparentX = Math.cos(awa) * aws
+          var apparentY = Math.sin(awa) * aws
+          angle = Math.atan2(apparentY, -speed + apparentX)
+          speed = Math.sqrt(
+            Math.pow(apparentY, 2) + Math.pow(-speed + apparentX, 2)
+          )
+
+          if (aws < 1e-9) {
+            angle = awa
+          }
+        }
+
+        return [
+          { path: 'environment.wind.angleTrueWater', value: angle },
+          { path: 'environment.wind.speedTrue', value: speed }
+        ]
+      }
     }
-  }
+  ]
 }
