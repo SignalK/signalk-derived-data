04a518b044fcefe2c8897649e79090b4493f4468
panaaj <38519157+panaaj@users.noreply.github.com>
2025-10-01 13:37:28 +0930

    Add utility functions and tests

--- test/test.js
+++ test/test.js
@@ -3,13 +3,20 @@ const fs = require('fs')
 const _ = require('lodash')
 const chai = require('chai')
 chai.Should()
-chai.use(require('chai-json-equal'));
+chai.use(require('chai-json-equal'))
+const expect = require('chai').expect
+const {
+  isCompassAngle,
+  isPosition,
+  formatCompassAngle,
+  degreesToRadians
+} = require('../utils')
 
 let selfData = {}
 
 const app = {
-  debug: () => { },
-  getSelfPath: (path) => _.get(selfData, path)
+  debug: () => {},
+  getSelfPath: path => _.get(selfData, path)
 }
 
 const plugin = {
@@ -19,7 +26,7 @@ const plugin = {
   air: ['outside']
 }
 
-function load_calcs() {
+function load_calcs () {
   fpath = path.join(__dirname, '../calcs')
   files = fs.readdirSync(fpath)
   return files
@@ -32,24 +39,110 @@ function load_calcs() {
     })
 }
 
+describe('Test Utility functions', function () {
+  it(`isCompassAngle(Math.PI)`, done => {
+    const n = isCompassAngle(Math.PI)
+    n.should.equal(true)
+    done()
+  })
+  it(`isCompassAngle(0)`, done => {
+    const n = isCompassAngle(0)
+    n.should.equal(true)
+    done()
+  })
+  it(`isCompassAngle(-1)`, done => {
+    const n = isCompassAngle(-1)
+    n.should.equal(false)
+    done()
+  })
+  it(`isCompassAngle(10)`, done => {
+    const n = isCompassAngle(10)
+    n.should.equal(false)
+    done()
+  })
 
+  it(`isPosition({latitude: -10, longitude: 12})`, done => {
+    const n = isPosition({ latitude: -10, longitude: 12 })
+    n.should.equal(true)
+    done()
+  })
+  it(`isPosition({latitude: 10, longitude: -12})`, done => {
+    const n = isPosition({ latitude: 10, longitude: -12 })
+    n.should.equal(true)
+    done()
+  })
+  it(`isPosition({latitude: -100, longitude: 12})`, done => {
+    const n = isPosition({ latitude: -100, longitude: 12 })
+    n.should.equal(false)
+    done()
+  })
+  it(`isPosition({latitude: -10, longitude: 182})`, done => {
+    const n = isPosition({ latitude: -100, longitude: 182 })
+    n.should.equal(false)
+    done()
+  })
+  it(`isPosition({latitude: null, longitude: 182})`, done => {
+    const n = isPosition({ latitude: null, longitude: 182 })
+    n.should.equal(false)
+    done()
+  })
+  it(`isPosition({latitude: -10, longitude: null})`, done => {
+    const n = isPosition({ latitude: -10, longitude: null })
+    n.should.equal(false)
+    done()
+  })
+  it(`isPosition(null)`, done => {
+    const n = isPosition(null)
+    n.should.equal(false)
+    done()
+  })
+  it(`isPosition(undefined)`, done => {
+    const n = isPosition(undefined)
+    n.should.equal(false)
+    done()
+  })
+
+  it(`formatCompassAngle(2.13)`, done => {
+    const n = formatCompassAngle(2.13)
+    n.should.equal(2.13)
+    done()
+  })
+  it(`formatCompassAngle(375)`, done => {
+    const n = formatCompassAngle(degreesToRadians(375)).toFixed(4)
+    n.should.equal(degreesToRadians(15).toFixed(4))
+    done()
+  })
+  it(`formatCompassAngle(-10)`, done => {
+    const n = formatCompassAngle(degreesToRadians(-10)).toFixed(4)
+    n.should.equal(degreesToRadians(350).toFixed(4))
+    done()
+  })
+  it(`formatCompassAngle(abc)`, done => {
+    const n = formatCompassAngle('abc')
+    expect(n).to.equal(null)
+    done()
+  })
+})
 
 describe('derived data converts', function () {
   let calcs = load_calcs()
 
   calcs.forEach(calci => {
-    (_.isArray(calci) ? calci : [calci]).forEach(calc => {
+    ;(_.isArray(calci) ? calci : [calci]).forEach(calc => {
       if (calc.tests) {
         calc.tests.forEach((test, idx) => {
-          it(`${calc.title}[${idx}] works`, (done) => {
+          it(`${calc.title}[${idx}] works`, done => {
             selfData = test.selfData || {}
             let res = calc.calculator.apply(null, test.input)
             if (test.expected) {
               res.should.jsonEqual(test.expected)
             } else if (test.expectedRange) {
-              res[0].value.should.closeTo(test.expectedRange[0].value, test.expectedRange[0].delta)
+              res[0].value.should.closeTo(
+                test.expectedRange[0].value,
+                test.expectedRange[0].delta
+              )
             } else {
-              (typeof res).should.equal('undefined')
+              ;(typeof res).should.equal('undefined')
             }
             done()
           })
@@ -58,4 +151,3 @@ describe('derived data converts', function () {
     })
   })
 })
-
---
+++ utils.js
@@ -0,0 +1,57 @@
+/**
+ * @description Tests that supplied value is a number
+ * @param value Value to test
+ * @returns true if value is a number
+ */
+const isNumeric = value =>
+  typeof value === 'number' && !isNaN(value) && value !== Infinity
+
+/**
+ * @description Tests that supplied value is between 0 & 2*Pi
+ * @param value Value in radians
+ * @returns true if value is between 0 & 2*Pi
+ */
+export function isCompassAngle (value) {
+  if (isNumeric(value)) {
+    const twoPi = Math.PI * 2
+    return value >= 0 && value < twoPi
+  } else {
+    return false
+  }
+}
+
+/**
+ * @description Ensures supplied value is between 0 & 2*Pi
+ * @param value Value in radians
+ * @returns Value between 0 & 2*Pi
+ */
+export function formatCompassAngle (value) {
+  if (isNumeric(value)) {
+    const twoPi = Math.PI * 2
+    if (value >= twoPi) return value - twoPi
+    if (value < 0) return twoPi + value
+    return value
+  } else {
+    return null
+  }
+}
+
+/**
+ * @description Tests that supplied value is a valid Signal K position
+ * @param value Value to test
+ * @returns true if value is a valid Signal K position
+ */
+export function isPosition (value) {
+  if (value && isNumeric(value.latitude) && isNumeric(value.longitude)) {
+    return (
+      value.latitude >= -90 &&
+      value.latitude <= 90 &&
+      (value.longitude >= -180 && value.longitude <= 180)
+    )
+  } else {
+    return false
+  }
+}
+
+export const degreesToRadians = value => Math.PI / 180 * value
+export const radiansToDegrees = value => 180 / Math.PI * value
